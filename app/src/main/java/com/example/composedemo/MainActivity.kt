06b71814ee99b31dd3e07961449f76e26873a465
saikalyan <daroju.sai@nobroker.in>
2023-10-08 13:27:33 +0530

    recycler view using lazy colum

--- app/src/main/java/com/example/composedemo/MainActivity.kt
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -3,20 +3,23 @@ package com.example.composedemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composedemo.ui.theme.ComposeDemoTheme
-import com.example.composedemo.ui_blocks.ComposeCard
 import com.example.composedemo.ui_blocks.ConstraintComposeCard
 
 class MainActivity : ComponentActivity() {
@@ -24,19 +27,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MainScreenV2()
-        }
-    }
-}
-
-@Composable
-@Preview(showSystemUi = true, showBackground = true, fontScale = 1F)
-private fun MainScreen() {
-    ComposeDemoTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.surface
-        ) {
-            ComposeCard()
         }
     }
 }
@@ -51,11 +41,13 @@ private fun MainScreenV2() {
                 .fillMaxWidth()
                 .wrapContentHeight(Alignment.Top)
                 .padding(10.dp),
-            color = MaterialTheme.colorScheme.surface,
-            shadowElevation = 3.dp,
-            shape = RoundedCornerShape(7.dp)
+            color = MaterialTheme.colorScheme.surface
         ) {
-            ConstraintComposeCard()
+            LazyColumn() {
+                items(count = 15) {
+                    ConstraintComposeCard()
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/composedemo/ui_blocks/ConstraintComposeCard.kt
+++ app/src/main/java/com/example/composedemo/ui_blocks/ConstraintComposeCard.kt
@@ -6,11 +6,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -29,9 +32,12 @@ fun ConstraintComposeCard() {
     ConstraintLayout(
         modifier = Modifier
             .fillMaxWidth()
+            .padding(10.dp)
+            .clip(RoundedCornerShape(7.dp))
             .background(Color.Black)
             .wrapContentHeight(align = Alignment.Top)
             .padding(10.dp)
+            .shadow(3.dp, RoundedCornerShape(7.dp))
     ) {
 
         val (imageRef, textView1, textView2, textView3, textView4, textView5, textView6, textView7, textView8,
@@ -121,7 +127,6 @@ fun ConstraintComposeCard() {
                 top.linkTo(textView8.bottom, margin = 5.dp)
                 start.linkTo(textView8.start)
             })
-
     }
 }
 
